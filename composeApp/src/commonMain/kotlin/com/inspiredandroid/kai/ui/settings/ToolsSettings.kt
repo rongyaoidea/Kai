94c32643d44e035ef9c5e04ad35411357ae881ed
rongyaoidea <rongyaoidea@users.noreply.github.com>
2026-09-15 17:55:05 +0000

    style: spotlessApply import order in ToolsSettings

--- composeApp/src/commonMain/kotlin/com/inspiredandroid/kai/ui/settings/ToolsSettings.kt
+++ composeApp/src/commonMain/kotlin/com/inspiredandroid/kai/ui/settings/ToolsSettings.kt
@@ -1,7 +1,6 @@
 package com.inspiredandroid.kai.ui.settings
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -13,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
